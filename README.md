45c31764c933be1c156b555cbde390296754aeff
smyrnof <smyrnof@gmail.com>
2017-02-07 18:03:25 +0300

    latest

--- README.md
+++ README.md
@@ -1,32 +1,69 @@
 # ngx_pagespeed_bashscript
 
-This script prepare for CentOS 6 or higher. It downloads and installs nginx server with some modules automaticly.
+This script prepare for CentOS 6 or higher. It downloads and installs nginx server with google page speed and some other modules automaticly.
 
 Modules;
 
+--add-module=ngx_pagespeed
+
 --with-file-aio 
+
 --with-ipv6 
+
 --with-http_ssl_module 
+
 --with-http_spdy_module 
+
 --with-http_realip_module 
+
 --with-http_addition_module 
+
 --with-http_auth_request_module 
+
 --with-http_xslt_module 
+
 --with-http_image_filter_module 
+
 --with-http_geoip_module 
+
 --with-http_sub_module 
+
 --with-http_dav_module 
+
 --with-http_flv_module 
+
 --with-http_mp4_module 
+
 --with-http_gunzip_module 
+
 --with-http_gzip_static_module 
+
 --with-http_random_index_module 
+
 --with-http_secure_link_module 
+
 --with-http_degradation_module 
+
 --with-http_stub_status_module 
+
 --with-http_perl_module 
+
 --with-mail 
+
 --with-mail_ssl_module 
+
 --with-pcre
 
-If you want to install manually, you can read this tutorial https://www.aligokayduman.com/427-nginx-pagespeed-modul-kurulumu/ (turkish)
+If you want to install automatically, you can run this code on commad line
+
+bash <(curl -f -L -sS https://raw.githubusercontent.com/aligokayduman/ngx_pagespeed_bashscript/master/install.sh) page_speed_version nginx_version $HOME
+
+google page speed module versions https://modpagespeed.com/doc/release_notes
+
+nginx versions http://nginx.org/en/download.html
+
+example: bash <(curl -f -L -sS https://raw.githubusercontent.com/aligokayduman/ngx_pagespeed_bashscript/master/install.sh) 1.12.34.2 1.8.1 $HOME
+
+If you want to install manually, you can read this tutorial 
+
+https://www.aligokayduman.com/427-nginx-pagespeed-modul-kurulumu/ (turkish)
--- install.sh
+++ install.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
 
-yum install gcc-c++ pcre-devel zlib-devel make unzip -y
+sudo yum install gcc-c++ pcre-devel zlib-devel make unzip -y
 
 cd ~
-rpm --import https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/slc6X/i386/RPM-GPG-KEY-cern
-wget -O /etc/yum.repos.d/slc6-devtoolset.repo https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/devtoolset/slc6-devtoolset.r$
-yum install devtoolset-2-gcc-c++ devtoolset-2-binutils -y
+sudo rpm --import https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/slc6X/i386/RPM-GPG-KEY-cern
+sudo wget -O /etc/yum.repos.d/slc6-devtoolset.repo https://linux.web.cern.ch/linux/scientific6/docs/repository/cern/devtoolset/slc6-devtoolset.repo
+sudo yum install devtoolset-2-gcc-c++ devtoolset-2-binutils -y
 
 PS_NGX_EXTRA_FLAGS="--with-cc=/opt/rh/devtoolset-2/root/usr/bin/gcc"
 
 cd ~
 NPS_VERSION=$1
-wget https://github.com/pagespeed/ngx_pagespeed/archive/v${NPS_VERSION}-beta.zip
-unzip v${NPS_VERSION}-beta.zip
+sudo wget https://github.com/pagespeed/ngx_pagespeed/archive/v${NPS_VERSION}-beta.zip
+sudo unzip v${NPS_VERSION}-beta.zip
 cd ngx_pagespeed-${NPS_VERSION}-beta/
 psol_url=https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
 [ -e scripts/format_binary_url.sh ] && psol_url=$(scripts/format_binary_url.sh PSOL_BINARY_URL)
-wget ${psol_url}
-tar -xzvf $(basename ${psol_url})
+sudo wget ${psol_url}
+sudo tar -xzvf $(basename ${psol_url})
 
-yum install openssl-devel libxslt-devel libxml2-devel gd-devel perl-ExtUtils-Embed geoip-devel google-perftools-devel -y
+sudo yum install openssl-devel libxslt-devel libxml2-devel gd-devel perl-ExtUtils-Embed geoip-devel google-perftools-devel -y
 
 cd ~
 NGINX_VERSION=$2
-wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
-tar -xvzf nginx-${NGINX_VERSION}.tar.gz
+sudo wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+sudo tar -xvzf nginx-${NGINX_VERSION}.tar.gz
 cd nginx-${NGINX_VERSION}/
-./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.l$
-make
-make install
+./configure --prefix=/usr/share/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log
+            --http-log-path=/var/log/nginx/access.log --http-client-body-temp-path=/var/lib/nginx/tmp/client_body
+            --http-proxy-temp-path=/var/lib/nginx/tmp/proxy --http-fastcgi-temp-path=/var/lib/nginx/tmp/fastcgi
+            --http-uwsgi-temp-path=/var/lib/nginx/tmp/uwsgi --http-scgi-temp-path=/var/lib/nginx/tmp/scgi --pid-path=/var/run/nginx.pid
+            --lock-path=/var/lock/subsys/nginx --user=nginx --group=nginx --with-file-aio --with-ipv6 --with-http_ssl_module --with-http_spdy_module
+            --with-http_realip_module --with-http_addition_module --with-http_auth_request_module --with-http_xslt_module
+            --with-http_image_filter_module --with-http_geoip_module --with-http_sub_module --with-http_dav_module --with-http_flv_module
+            --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_random_index_module
+            --with-http_secure_link_module --with-http_degradation_module --with-http_stub_status_module --with-http_perl_module --with-mail
+            --with-mail_ssl_module --with-pcre --with-google_perftools_module --with-debug
+            --with-cc-opt='-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector --param=ssp-buffer-size=4 -m64 -mtune=generic'
+            --with-ld-opt=' -Wl,-E' --add-module=$3/ngx_pagespeed-${NPS_VERSION}-beta ${PS_NGX_EXTRA_FLAGS}
+sudo make
+sudo make install
 
-wget https://raw.githubusercontent.com/aligokayduman/ngx_pagespeed_bashscript/master/nginx -P /etc/init.d/
-chmod 755 /etc/init.d/nginx
+sudo rm -rf /etc/init.d/nginx
+sudo wget https://raw.githubusercontent.com/aligokayduman/ngx_pagespeed_bashscript/master/nginx -P /etc/init.d/
+sudo chmod 755 /etc/init.d/nginx
 
-service nginx start
-chkconfig nginx on
+sudo service nginx start
+sudo chkconfig nginx on
